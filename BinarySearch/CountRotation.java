aeebe529b34c079b7032ccbe1020c3510f07b0aa
aditya.pandey <aditya.pandey@medikabazaar.com>
2024-09-26 22:08:47 +0530

    fix bug CountRotation

--- BinarySearch/CountRotation.java
+++ BinarySearch/CountRotation.java
@@ -11,6 +11,9 @@ public class CountRotation {
         int arr3[] = new int[] { 7, 9, 11, 12, 15 };
         System.out.println("output : " + solve(arr3));
         System.out.println("binarySearch output : " + binarySearch(arr3));
+        int arr4[] = new int[] { 4,5,6,7,0,1,2 };
+        System.out.println("output : " + solve(arr4));
+        System.out.println("binarySearch output : " + binarySearch(arr4));
     }
 
     public static int solve(int[] arr) {
@@ -32,9 +35,9 @@ public class CountRotation {
             int next = (mid + 1) % n;
             if (arr[prev] > arr[mid] && arr[next] > arr[mid])
                 return mid;
-            else if (arr[start] > arr[mid])
+            else if (arr[0] > arr[mid])
                 end = mid - 1;
-            else if (arr[end] < arr[mid])
+            else if (arr[n-1] < arr[mid])
                 start = mid + 1;
         }
         return 0;
